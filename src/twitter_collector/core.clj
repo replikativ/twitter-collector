6b5e718ea1ce75cf7372571b086ebbbd670e391e
Christian Weilbach <christian_weilbach@web.de>
2016-08-30 03:44:30 +0200

    Only transact if the tweets are non-empty.

--- src/twitter_collector/core.clj
+++ src/twitter_collector/core.clj
@@ -30,8 +30,9 @@
 (defn store-tweets [stage pending]
   (go-try
    (let [st (.getTime (java.util.Date.))
-         tweet-txs [['add-tweets (vec (first (swap! pending (fn [[prev cur]] [cur '()]))))]]]
-     (when-not (empty? tweet-txs)
+         tweets (vec (first (swap! pending (fn [[prev cur]] [cur '()]))))
+         tweet-txs [['add-tweets tweets]]]
+     (when-not (empty? tweets)
        (<? (cs/transact! stage [user cdvcs-id] tweet-txs)))
      (when (< (rand) 0.01)
        (println "Date: " (java.util.Date.))
@@ -72,14 +73,21 @@
 
   (count (second @pending))
 
+  (require '[replikativ.p2p.fetch :refer [fetch]])
+
   (def client-store (<?? (new-fs-store "/media/void/1e843516-40ec-4b2b-83d2-c796ee312a59/twitter/")))
 
-  (def client (<?? (client-peer client-store)))
+  (def client (<?? (client-peer client-store :middleware fetch)))
 
   (def client-stage (<?? (create-stage! user client)))
   (<?? (cs/create-cdvcs! client-stage :id cdvcs-id))
 
+  (<?? (cs/merge! client-stage [user cdvcs-id]
+                  (seq (get-in @client-stage [user cdvcs-id :state :heads]))))
+
   (<?? (connect! client-stage "ws://127.0.0.1:9095"))
+
+  (<?? (connect! client-stage "ws://topiq.es:9095"))
 
   (keys @(:state (get-in @peer [:volatile])))
 
@@ -89,6 +97,8 @@
 
   (count (get-in @client-stage [user cdvcs-id :state :commit-graph]))
 
+  (get-in @client-stage [user cdvcs-id :state :heads])
+
   (count (get-in @stage [user cdvcs-id :state :commit-graph]))
 
   (require '[replikativ.crdt.cdvcs.realize :as r])
@@ -96,18 +106,31 @@
   (def hist
     (r/commit-history (get-in @client-stage [user cdvcs-id :state :commit-graph])
                       (first (get-in @client-stage [user cdvcs-id :state :heads]))))
-
+ 
   (require '[clojure.core.async :as async]
            '[konserve.core :as k])
 
   (def commits
-    (->> hist
-         (map (fn [id] (k/get-in mem-store [id])))
+    (->> (take 10 hist)
+         (map #(k/get-in client-store [%]))
          async/merge
          (async/into [])
-         <??))
+         <??
+         ))
 
-  (take 10 commits)
+  (filter nil? commits)
+
+  (map (comp keys second) commits)
+  (map (fn [{[[_ v]] :transactions :as elem}] (if-not v elem)) commits)
+
+  (->> commits
+       (map (fn [{[[_ v]] :transactions :as elem}] v))
+       (filter identity)
+       (map #(go-try [% (<? (k/get-in client-store [%]))]))
+       async/merge
+       (async/into [])
+       <??
+       (map first))
 
   (<?? (k/get-in mem-store [(first hist)]))
 
