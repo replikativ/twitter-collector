bf227daea6ce0a2360450391304ab6b7ae8a19be
Christian Weilbach <christian_weilbach@web.de>
2016-09-13 00:10:27 +0200

    Use new streaming support with Datomic.

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.replikativ/replikativ "0.2.0-beta2"]
+                 [io.replikativ/replikativ "0.2.0-SNAPSHOT"]
                  [io.replikativ/gezwitscher "0.2.0-SNAPSHOT"]
-                 [com.fzakaria/slf4j-timbre "0.3.1"]]
+                 [com.fzakaria/slf4j-timbre "0.3.1"]
+                 [com.datomic/datomic-free "0.9.5394"]]
 
   :main twitter-collector.core)
--- src/twitter_collector/client.clj
+++ src/twitter_collector/client.clj
@@ -8,39 +8,79 @@
             [konserve.filestore :refer [new-fs-store]]
             [clojure.core.async :as async]
             [full.async :refer [<??]]
-            [replikativ.stage :as s]))
+            [replikativ.stage :as s]
+            [konserve.core :as k]
+            [replikativ.crdt.cdvcs.stage :as cs]
+            [datomic.api :as d]))
 
+;; replikativ
 
-(def client-store (<?? (new-fs-store "/media/void/1e843516-40ec-4b2b-83d2-c796ee312a59/twitter/")))
+;; better not to use a memory store with gib of data ;)
+(def client-store (<?? (new-fs-store "/tmp/foo"
+                                     #_"/media/void/1e843516-40ec-4b2b-83d2-c796ee312a59/twitter/")))
 
-;; do not crypto hash (is default) for higher throughput (only do this for
-;; connections with ;; trusted peers)
-(def client (<?? (client-peer client-store :middleware fetch)))
+(def client (<?? (client-peer client-store)))
 
 (def client-stage (<?? (create-stage! user client)))
 (<?? (cs/create-cdvcs! client-stage :id cdvcs-id))
 
+;; in memory
+
 (def tweets (atom []))
 
-(def close-stream (r/stream-into-atom! client-stage [user cdvcs-id]
-                                       {'add-tweets (fn [old txs]
-                                                      ;; doall is here to free the txs memory as we go
-                                                      (swap! old into (doall (map :text txs)))
-                                                      old)}
-                                       tweets))
+(def atom-stream (r/stream-into-identity! client-stage [user cdvcs-id]
+                                          {'add-tweets (fn [old txs]
+                                                         ;; doall is here to free the txs memory as we go
+                                                         (swap! old into (doall (map :text txs)))
+                                                         old)}
+                                          tweets))
+
+
+;; streaming into datomic
+
+(def db-uri "datomic:mem://trail")
+
+(d/create-database db-uri)
+
+(comment
+  (d/delete-database "datomic:mem://trail"))
+
+(def conn (d/connect db-uri))
+
+@(d/transact conn (read-string (slurp "schema.edn")))
+
+(defn tweet-txs [txs]
+  (mapv (fn [{:keys [text]}]
+          {:db/id
+           (d/tempid :db.part/user)
+           :tweet/text text})
+        txs))
+
+
+(def datomic-stream (r/stream-into-identity! client-stage
+                                             [user cdvcs-id]
+                                             {'add-tweets (fn [conn txs]
+                                                            @(d/transact conn (tweet-txs txs))
+                                                            conn)}
+                                             conn
+                                             :applied-log :datomic-analysis
+                                             ;; not tested yet
+                                             :reset-fn (fn [old-conn] (prn "Resetting Datomic!")
+                                                         (d/delete-database db-uri)
+                                                         (def conn (d/connect db-uri))
+                                                         @(d/transact conn (read-string (slurp "schema.edn")))
+                                                         conn)))
+
+
+
+
 
 
 (comment
+  ;; test server, might be broken
   (<?? (connect! client-stage "ws://topiq.es:9095"))
 
-  (require '[konserve.core :as k])
-  (require '[replikativ.crdt.cdvcs.stage :as cs])
-
-  (<?? (cs/transact! client-stage [user cdvcs-id] [['+ 1]]))
-
   (<?? (k/get-in client-store [(last (<?? (k/get-in client-store [[user cdvcs-id :log]])))]))
-
-  (async/close! close-stream)
 
   (let [{{new-heads :heads
           new-commit-graph :commit-graph} :op :as op} {:heads [1 2 3]}]
@@ -53,6 +93,21 @@
 
   (count (filter (fn [s] (re-find #"racist" s)) @tweets))
 
-  (async/close! close-stream)
-  ;; flush
-  (while (async/poll! close-stream)))
+  (async/close! atom-stream)
+
+  ;; datomic analysis
+  (d/q '[:find ?t
+         :where
+         [?tw :tweet/text ?t]]
+       (d/db conn))
+
+  ;; test tweets
+  (<?? (cs/transact! client-stage [user cdvcs-id] [['add-tweets [{:text "Foo bar"}]]
+                                                   ['add-tweets [{:text "More foo"}]]]))
+
+  (async/close! datomic-stream)
+  (<?? (k/assoc-in client-store [:datomic-analysis] nil)) ;; reset log
+  (<?? (k/log client-store :datomic-analysis))
+
+
+  )
